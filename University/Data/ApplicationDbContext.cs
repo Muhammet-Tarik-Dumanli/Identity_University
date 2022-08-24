078c43818e095b24a61d147de4f12f67194aa294
MuhammetTarik <mtdumanli@gmail.com>
2022-08-24 17:33:02 +0300

    Add project files.

---
+++ University/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace University.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmentLesson> DepartmentLessons { get; set; }
+    }
+}
